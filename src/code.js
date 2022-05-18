16222c7044b273e902dbbfab304dba0a8d9bb2b1
Milian Mori <milian@milianmori.com>
2022-05-18 19:39:20 +0200

    play kick pattern

--- src/code.js
+++ src/code.js
@@ -1,7 +1,7 @@
 "use strict";
 //import * as Tone from 'tone';
 
-import { start, MidiClass, Oscillator, Gain, Transport, AmplitudeEnvelope, Synth, Offline, ToneAudioBuffer, Player} from 'tone';
+import { start, MidiClass, Part, Oscillator, Gain, Transport, AmplitudeEnvelope, Synth, Offline, ToneAudioBuffer, Player} from 'tone';
 import * as Nexus from 'nexusui';
 
 //import files
@@ -22,12 +22,15 @@ const bufferBass101 = new ToneAudioBuffer();
 const bufferBass102 = new ToneAudioBuffer();
 const bufferBass103 = new ToneAudioBuffer();
 
+//initialize player
+const playerKick = new Player(bufferKick1).toDestination();
+
 //Sequencer Interface
 var sequencer = new
 Nexus.Sequencer('#seq',{
-    'size': [750,150],
+    'size': [750,200],
     'mode': 'toggle',
-    'rows': 5,
+    'rows': 6,
     'columns': 48,
    });
 sequencer.colorize("fill","#808080")
@@ -48,7 +51,8 @@ const synth = new Synth().toDestination();
 
 Transport.bpm.value = 80;
 
-const startButton = document.getElementById('start');
+const generateButton = document.getElementById('generate');
+const playButton = document.getElementById('play');
 //const stopButton = document.getElementById('stop');
 //const newRandomGainsButton = document.getElementById('init');
 
@@ -73,7 +77,7 @@ function randomGains (index){
 };
 
 const env = new AmplitudeEnvelope({
-    attack: 0.1,
+    attack: 0.0,
     decay: 0.2,
     sustain: 1.0,
     release: 0.8,
@@ -130,10 +134,14 @@ function triggerABS(array)
 
 }
 
-
-
+/*
+1. Rule: every trigger lands on even index incl. 0:             [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
+2. Rule: every trigger has min 4 tringgers distance:            [1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0]
+3. Rule: expertion: last trigger can have 2 trigger dicance:    [1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0]
+4. Rule: if there is a trigger on the 2nd last trigger of the bar before, the trigger cant land on the 2nd trigger becuase of 2. Rule: Bar1: [1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0], Bar2: [0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0]*/
 function kickRhythm(array,flag){
 
+    //count how much triggers are in array
     var count = 0;
     array.forEach((e,i) =>{
         if(e===1) count++;
@@ -143,6 +151,7 @@ function kickRhythm(array,flag){
     var arrayABS = [];
     while (true)
     {
+        //1. Rule
         while(true) {
             array = shuffle(array);
             if (array.every((e,i) => {
@@ -153,17 +162,15 @@ function kickRhythm(array,flag){
             }
         }
         
-        console.log(array);
+        console.log("Kick: " + array);
 
+        //2. Rule
         arrayABS = triggerABS(array);
 
-        if(flag === 1){
-            console.log('A');
+        if(flag === 1){ // 4. Rule
             if(array[0] === 0){
-                console.log('B');
                 if (arrayABS.every((e,i) => {
-                    if(e < 4 && i === count-1){
-                        console.log('hello');
+                    if(e < 4 && i === count-1){ // 3. Rule
                         return true
                     }
                     else if(e < 4) return false;
@@ -174,11 +181,9 @@ function kickRhythm(array,flag){
             }
         }
 
-        else if (flag === 0){
-            console.log('C');
+        else if (flag === 0){ // 4. Rule
             if (arrayABS.every((e,i) => {
-                if(e < 4 && i === count-1){
-                    console.log('hello');
+                if(e < 4 && i === count-1){ // 3.Rule
                     return true
                 }
                 else if(e < 4) return false;
@@ -191,12 +196,99 @@ function kickRhythm(array,flag){
 
 
     }
+    
 return array;
 }
 
-/*function bassRhythm1(){
+/*
+1. Rule: every trigger lands on even index incl. 0:             [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
+2. Rule: every trigger has min 4 tringgers distance:            [1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0]
+3. Rule: expertion: last trigger can have 2 trigger dicance:    [1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0]
+4. Rule: if there is a trigger on the 2nd last trigger of the bar before, the trigger cant land on the 2nd trigger becuase of 2. Rule: Bar1: [1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0], Bar2: [0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0]
+5. Rule: tirgger can not land on Kick tirgger*/
+function bassRhythm(array, fullKickOutput,flag,offset){
 
-};*/
+    //count how much triggers are in array
+    var count = 0;
+    array.forEach((e,i) =>{
+        if(e===1) count++;
+    });
+    console.log(count);
+
+    var arrayABS = [];
+
+    while(true){
+        while (true)
+        {
+            //1. Rule
+            while(true) {
+                array = shuffle(array);
+                if (array.every((e,i) => {
+                    if(e === 1) return i % 2 === 0;
+                    else return true; // wenns kein schlag ist alles gut, soll weither ggehen
+                })) {
+                    break;
+                }
+            }
+            
+            console.log("Bass: " + array);
+    
+            //2. Rule
+            arrayABS = triggerABS(array);
+    
+            if(flag === 1){ // 4. Rule
+                if(array[0] === 0){
+                    if (arrayABS.every((e,i) => {
+                        if(e < 4 && i === count-1){ // 3. Rule
+                            return true
+                        }
+                        else if(e < 4) return false;
+                        else return true;
+                    })) {
+                        break;
+                    }
+                }
+            }
+    
+            else if (flag === 0){ // 4. Rule
+                if (arrayABS.every((e,i) => {
+                    if(e < 4 && i === count-1){ // 3.Rule
+                        return true
+                    }
+                    else if(e < 4) return false;
+                    else return true;
+                })) {
+                    break;
+                }
+            }
+    
+    
+    
+        }
+
+        // 5. Rule 
+        if (array.every((e,i) => {
+
+            if(array[i] === 1 && array[i] === fullKickOutput[offset][i])
+            {
+                console.log('false');
+                return false;
+            }
+            else
+            {   
+                //console.log(fullgeneratedKick[i+offset]);
+                console.log('true');
+                return true;
+            }
+    })) {
+        break;
+    }
+
+    }
+
+    return array;
+
+}
 
 function checklastTrigger(array){
     if(array[14] === 1 ) return 1;
@@ -204,7 +296,8 @@ function checklastTrigger(array){
 }
 
 
-startButton.addEventListener('click', async () => {
+
+generateButton.addEventListener('click', async () => {
     await start();
     Transport.start();
 
@@ -220,14 +313,17 @@ startButton.addEventListener('click', async () => {
     const bar3Kick = [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
     var flag = 0;
-    const generatedBar1 = kickRhythm(bar1Kick,flag);
+    const generatedBar1Kick = kickRhythm(bar1Kick,flag);
     flag = checklastTrigger(bar1Kick);
-    const generatedBar2 = kickRhythm(bar2Kick,flag);
+    const generatedBar2Kick = kickRhythm(bar2Kick,flag);
     flag = checklastTrigger(bar2Kick);
-    const generatedBar3 = kickRhythm(bar3Kick,flag);
+    const generatedBar3Kick = kickRhythm(bar3Kick,flag);
 
-    const fullgeneratedKick = generatedBar1.concat(generatedBar2,generatedBar3);
-    sequencer.matrix.set.row(2,fullgeneratedKick);
+    const fullgeneratedKick = generatedBar1Kick.concat(generatedBar2Kick,generatedBar3Kick);
+    const output = generatedBar1Kick.concat(generatedBar2Kick,generatedBar3Kick);
+    sequencer.matrix.set.row(2,output);
+
+    const fullKickOutput = [generatedBar1Kick, generatedBar2Kick, generatedBar3Kick];
 
     //Generate Bass
     const bar1Bass = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
@@ -235,18 +331,33 @@ startButton.addEventListener('click', async () => {
     const bar3Bass = [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
     var flag = 0;
-    const generatedBar1 = kickRhythm(bar1Kick,flag);
-    flag = checklastTrigger(bar1Kick);
-    const generatedBar2 = kickRhythm(bar2Kick,flag);
-    flag = checklastTrigger(bar2Kick);
-    const generatedBar3 = kickRhythm(bar3Kick,flag);
+    const generatedBar1Bass = bassRhythm(bar1Bass,fullKickOutput,flag,0);
+    flag = checklastTrigger(bar1Bass);
+    const generatedBar2Bass = bassRhythm(bar2Bass,fullKickOutput,flag,1);
+    flag = checklastTrigger(bar2Bass);
+    const generatedBar3Bass = bassRhythm(bar3Bass,fullKickOutput,flag,2);
 
+    const fullgeneratedBass = generatedBar1Bass.concat(generatedBar2Bass,generatedBar3Bass);
+    sequencer.matrix.set.row(3,fullgeneratedBass);
 
-    
+    const fullBassOutput = [generatedBar1Kick, generatedBar2Kick, generatedBar3Kick];
 
+    // Generate Rhythm Figure 1
+    const bar1RhythmFigure1 = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    const bar2RhythmFigure1 = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    const bar3RhythmFigure1 = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
- 
+    var flag = 0;
+    const generatedRhythmFigure1Bar1 = bassRhythm(bar1RhythmFigure1,fullBassOutput,flag,0);
+    flag = checklastTrigger(bar1Bass);
+    const generatedRhythmFigure1Bar2 = bassRhythm(bar2RhythmFigure1,fullBassOutput,flag,1);
+    flag = checklastTrigger(bar2Bass);
+    const generatedRhythmFigure1Bar3 = bassRhythm(bar3RhythmFigure1,fullBassOutput,flag,2);
 
+    const fullgeneratedRhythmFigure1 = generatedRhythmFigure1Bar1.concat(generatedRhythmFigure1Bar2,generatedRhythmFigure1Bar3);
+    sequencer.matrix.set.row(4,fullgeneratedRhythmFigure1);
+
+    // load audio samples
     await bufferKick1.load(audioFileUrlKick1);
     await bufferKick2.load(audioFileUrlKick2);
     await bufferKick3.load(audioFileUrlKick3);
@@ -255,27 +366,60 @@ startButton.addEventListener('click', async () => {
     await bufferBass102.load(audioFileUrlBass102);
     await bufferBass103.load(audioFileUrlBass103);
 
+    //play sounds
+    function play(time, note) {
+        console.log('trigger', time, note)
+        playerKick.start(time);
+    };
+
+    console.log(fullgeneratedKick);
+    var current = 0;
+    var pattern = fullgeneratedKick
+        .map((e,i) => {
+            if(i<=15){
+                current = i;
+                return [e, `0:${Math.floor(current/4)}:${i%4}`]
+            }
+            else if(i>=16 && i<32) {
+                current = i-16;
+                return [e, `1:${Math.floor(current/4)}:${i%4}`]
+            } 
+            else{
+                current = i-32;
+                return [e, `2:${Math.floor(current/4)}:${i%4}`]
+            }
+        });
+
+    console.log(pattern);
+
+    pattern = pattern.filter(e => e[0] === 1)
+
+    console.log(pattern);
+
+    pattern = pattern.map((e,i) => [e[1], `C${i}`]);
+
+    console.log(pattern);
+
+    //console.log(pattern);
+    const part = new Part(play, pattern);
+
+    Transport.bpm.value = 150;
+    //Transport.start();
+
+    
+    //part.length = 48;
+    part.loopEnd = '3:0:0';
+    part.loop = true;
+    console.log(part.loopEnd);
+    part.start();
+    
+
+});
 
 
-    const player = new Player(bufferKick1).toDestination();
-    player.loop = true;
-    //player.start();
 
-    gains = gains.map((item,index) => {
-        return (new Gain({gain: exponentialGains(index)}).connect(env)); //connect(env)
-    });
-
-    /*oscillator1 = oscillator1.map((item,index) => {
-        return (new Oscillator({frequency: frequencies[index], type: "sine"})).connect(gains[index]).start();
-    });*/
-
-    //console.log(exponentialGainValues);
-    //console.log(arrayTest);
-    //console.log(randomGainValues);
-
-  });
-
+/*
 Transport.scheduleRepeat((time) => {
 	// use the callback time to schedule events
-	env.triggerAttackRelease('16n',time);
-}, "8n");
+	env.triggerAttackRelease('8n',time);
+}, "4n");*/
--- src/fragen.md
+++ src/fragen.md
@@ -2,3 +2,8 @@ Was macht?:
 
 Add Homebrew's pip path to your PYTHONPATH environment variable (you probably should add this to some sort of shell initialization file like ~/.bashrc or ~/.zshrc)
   export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
+
+
+mm_instru_test_1 checken: wieso ging das nicht mit fullgeneratedkick?
+
+const fullgeneratedKick = generatedBar1Kick.concat(generatedBar2Kick,generatedBar3Kick);
